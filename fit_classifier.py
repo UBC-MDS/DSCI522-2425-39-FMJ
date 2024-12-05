0dcd3957048262d53edfe4647de988029e15f26a
Forsythia-olive <forgiveagbesi7478@gmail.com>
2024-12-04 16:52:57 -0800

    Added the classifier script

---
+++ fit_classifier.py
@@ -0,0 +1,130 @@
+# fit_age_group_classifier.py
+# author: Forgive Agbesi
+# date: 2024-12-3
+
+import click
+import os
+import altair as alt
+import numpy as np
+import pandas as pd
+import pickle
+from sklearn.dummy import DummyClassifier  
+from sklearn.compose import make_column_transformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler 
+from sklearn.model_selection import cross_validate
+from deepchecks.tabular import Dataset
+from sklearn import set_config
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.pipeline import make_pipeline
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning, module="deepchecks")
+
+
+@click.command()
+@click.option('--X-training-data', type=str, help="Path to training data")
+@click.option('--y-training-data', type=str, help="Path to training data")
+@click.option('--columns-to-drop', type=str, help="Optional: columns to drop")
+@click.option('--pipeline-to', type=str, help="Path to directory where the pipeline object will be written to")
+@click.option('--plot-to', type=str, help="Path to directory where the plot will be written to")
+@click.option('--seed', type=int, help="Random seed", default=123)
+
+def mean_cross_val_scores(model, X_train, y_train, **kwargs):
+    """
+    Returns mean and std of cross validation
+
+    Parameters  
+    ----------
+        model :
+            scikit-learn model
+        X_train : numpy array or pandas DataFrame
+            X in the training data
+        y_train :
+            y in the training data
+    Returns
+    ----------
+        pandas Series with mean scores from cross_validation
+    """
+    scores = cross_validate(model, X_train, y_train, **kwargs)
+    mean_scores = pd.DataFrame(scores).mean()
+    out_col = []
+
+    for i in range(len(mean_scores)):
+        out_col.append((mean_scores.iloc[i]))
+    return pd.Series(data=out_col, index=mean_scores.index)
+
+
+def main(X_training_data, y_training_data, preprocessor, pipeline_to, plot_to, seed):
+    '''Fits the age group classifier to the training data 
+    and saves the pipeline object.'''
+
+    np.random.seed(seed)
+    set_config(transform_output="pandas")
+
+    # read in data & preprocessor
+    X_train = pd.read_csv(X_training_data)
+    y_train = pd.read_csv(y_training_data)
+
+    # Transforming columns based on their type
+    numeric_features = ["bmi", "blood_glucose", "oral", "blood_insulin"]
+    binary_features = ["gender", "physical_activity", "diabetic"]
+
+    preprocessor = make_column_transformer(
+        (OneHotEncoder(sparse_output = False,
+                   drop='if_binary',dtype = int), binary_features),
+        (StandardScaler(), numeric_features)
+    )
+   
+
+    # tune model (here, use default hyper parameter values)
+    classifier_result = {}
+    dummy = DummyClassifier(random_state = 123)
+    dc_pipe = make_pipeline(preprocessor, dummy)
+    classifier_result['Dummy']= mean_cross_val_scores(dc_pipe, X_train, y_train, cv=5, 
+                                                        return_train_score=True)
+                                                        
+    
+
+
+    lr = LogisticRegression(random_state = 123)
+    lr_pipe = make_pipeline(preprocessor, lr)
+    classifier_result['Logistic']= mean_cross_val_scores(lr_pipe, X_train, y_train, cv=5, 
+                                                         return_train_score=True)
+                                                         
+    
+    
+
+    svc = SVC(random_state = 123)
+    svc_pipe = make_pipeline(preprocessor, svc)
+    classifier_result['SVC']= mean_cross_val_scores(svc_pipe, X_train, y_train, cv=5, 
+                                                    return_train_score=True)
+                                                         
+   
+    model_cv_score = pd.DataFrame(classifier_result).T
+    model_cv_score = model_cv_score.drop(columns=['fit_time', 'score_time'])
+
+
+    # save model_cv_score as image or file to be read in report
+    #
+    #
+    #
+    #
+    # 
+    # 
+    # 
+    # 
+    #
+    
+
+
+
+
+    
+    # fitting best model 
+    svc_pipe_fit = svc_pipe.fit(X_train, y_train)
+
+    with open(os.path.join(pipeline_to, "svc_classifier_pipeline.pickle"), 'wb') as f:
+        pickle.dump(svc_pipe_fit, f)
+
+if __name__ == '__main__':
+    main()
